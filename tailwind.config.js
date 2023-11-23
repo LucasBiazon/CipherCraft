5c70ebcd71dab834b9f4a16e033b4bbe0e09e2f9
Lucas Biazon Palma <lucasbiazonpalma@gmail.com>
2023-11-23 07:59:11 -0300

    Organization

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js, css}"],
+  content: ["./src/**/*.{html,js, css}",
+              "./index.html"],
   theme: {
     extend: {
       height:{
