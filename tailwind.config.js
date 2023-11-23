baccdb3896e16fc5f1e7a0d73351a713edab50f2
Lucas Biazon Palma <lucasbiazonpalma@gmail.com>
2023-11-23 01:46:02 -0300

    Finished Generator Style

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,14 @@ module.exports = {
   content: ["./src/**/*.{html,js, css}"],
   theme: {
     extend: {
+      height:{
+        'main': "calc(100% - 64px)"
+      },
+      dropShadow:{
+        '1xl': "0px 0px 50px -3px rgba(204,100,204,1)"
+      }
     },
   },
-  plugins: [ require("@tailwindcss/forms")],
+  plugins: [],
 }
 
